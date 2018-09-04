8bf834411e8ea19c12dd221c27e3f03ddf1409b7
squarepixel <chris@square2pixel.com>
2018-09-04 02:31:50 -0600

    Initial commit

---
+++ source/js/index.js
@@ -0,0 +1,3 @@
+// @codekit-prepend quiet "../../node_modules/jquery/dist/jquery.js";
+// @codekit-prepend quiet "../../node_modules/popper.js/dist/umd/popper.js";
+// @codekit-prepend quiet "../../node_modules/bootstrap/dist/js/bootstrap.js";
